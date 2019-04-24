6cf1ae44e44628339ad57fc50b0006795e0218ce
imedev4 <imedev4@imedev4s-macbook-pro.local>
2019-04-24 11:36:13 +0300

    Initial Commit

---
+++ panAndPinch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  panAndPinch
+//
+//  Created by imedev4 on 24/04/2019.
+//  Copyright © 2019 5W2H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
